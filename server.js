810ac0f9b1d97229ea43c3f3d63e8db8374f80f1
Markus <www.markushuber@gmail.com>
2020-04-09 19:17:24 +0200

    default port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socket = require('socket.io');
 
 const app = express();  // init server
-const server = app.listen(3000);  // run server
+const server = app.listen(process.env.PORT || 3000 );  // run server
 
 const io = socket(server);  // init socket
 
