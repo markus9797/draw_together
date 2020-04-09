12fce481a766a9ac8b4f2505a970d52841716378
Markus <www.markushuber@gmail.com>
2020-04-09 19:39:25 +0200

    store ellipses

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const server = app.listen(process.env.PORT || 3000 );  // run server
 
 const io = socket(server);  // init socket
 
+let users = []; // connected users
+let ellipses = []; // store the current painting
+
 
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
 
+    users.push(socket);
+    socket.emit('load', ellipses);
+
     socket.on('mouse', (data)=>{
         socket.broadcast.emit('mouse', data);
+        ellipses.push(data);
     });
 }
