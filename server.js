8e453f36f5e0f9d564814f944f5ab931ffec4b82
Markus <www.markushuber@gmail.com>
2020-04-09 23:50:02 +0200

    update draw

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = app.listen(process.env.PORT || 3000 );  // run server
 const io = socket(server);  // init socket
 
 let users = []; // connected users
-let ellipses = []; // store the current painting
+let lines = []; // store the current painting
 
 
 io.sockets.on('connection', newConnection);
@@ -17,17 +17,16 @@ function newConnection(socket) {
     users.push(socket);
 
     socket.on('load', ()=>{
-        console.log("sending load data...", ellipses)
-        socket.emit('loaded', ellipses);
+        socket.emit('loaded', lines);
     });
 
     socket.on('delete', ()=>{
-        ellipses = [];
+        lines = [];
         socket.broadcast.emit('deleted');
     });
 
     socket.on('mouse', (data)=>{
         socket.broadcast.emit('mouse', data);
-        ellipses.push(data);
+        lines.push(data);
     });
 }
