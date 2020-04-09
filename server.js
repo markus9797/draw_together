ba46ca47a52155f82a970f3eeb4a5cdff4a1e44b
Markus <www.markushuber@gmail.com>
2020-04-09 18:56:47 +0200

    remove logs

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();  // init server
+const server = app.listen(3000);  // run server
+
+const io = socket(server);  // init socket
+
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+
+    socket.on('mouse', (data)=>{
+        socket.broadcast.emit('mouse', data);
+    });
+}
