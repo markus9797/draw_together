86223642b06fc9d76ed54fbb68ac43c328782ad7
Markus <www.markushuber@gmail.com>
2020-04-10 05:33:32 +0200

    add chat

--- server.js
+++ server.js
@@ -9,12 +9,27 @@ const io = socket(server);  // init socket
 let users = []; // connected users
 let lines = []; // store the current painting
 
+let Players = [];
 
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
 
     users.push(socket);
+
+    socket.on('disconnect', function() {
+        console.log('Got disconnect!');
+        const username = users[socket].username;
+        let i = users.indexOf(socket);
+        users.splice(i, 1);
+
+        socket.broadcast.emit('removeUser', username);
+    });
+
+    socket.on('join', (username)=>{
+        users[socket].username = username;
+        socket.broadcast.emit('addUser', username)
+    });
 
     socket.on('load', ()=>{
         socket.emit('loaded', lines);
