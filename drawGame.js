658a201c2ca052046172e90a49a6e86233d13289
root <root@localhost.localdomain>
2020-04-12 19:44:01 +0200

    score system db

--- drawGame.js
+++ drawGame.js
@@ -16,54 +16,95 @@ class drawGame {
         this.sockets = init.sockets;
         this.lines = [];
         this.chat = [];
+        this.scores = {};
         this.players = init.players;
+        this.correct_guesses = 0;
+        this.total_players = init.players.length;
+        this.initScores();
+        this.initWordlist();
+        this.countdown = null;
+    }
+
+    loadDB(con, callback){
+        con.query(`SELECT sk_begriff, category, author.name 
+                     FROM skribbl, sk_author as author
+                     where skribbl.author = author.ID`, function (error, results, fields) {
+            if (error)
+                throw error;
+            let wordlist_new = [];
+            results.forEach(result => {
+                let data = {
+                    word: result.sk_begriff,
+                    category: result.category,
+                    author: result.name
+                };
+                wordlist_new.push(data);
+            });
+            callback(wordlist_new);
+        });
+    }
+
+    initWordlist(){
+        const mysql = require('mysql');
+        const con = mysql.createConnection({
+            host: process.env.db_name,
+            user: process.env.db_user,
+            password: process.env.db_password,
+            database : process.env.db_table
+        });
+        this.loadDB(con, (words)=>{
+            console.log ("LOADED WORDS");
+            this.wordlist = words;
+        })
+    }
+
+    initScores(){
+        for (let p = 0; p < this.total_players; p++){
+            this.scores[this.players[p].username] = 0; //todo: change to id, create jwt tokens
+        }
+        this.sockets.emit('loadedScores', this.scores);
     }
 
     time_clock(){
-        let countdown = setInterval(()=>{
+        this.countdown = setInterval(()=>{
             this.timeLeft -=1;
 
-            console.log(this.timeLeft);
-
             if (this.timeLeft === 0){
-                clearInterval(countdown);
-
-                let total_players = this.players.length;
-
-                console.log(this.current_player +1, " > ", total_players);
-
-                if (this.current_player +1 >= total_players){
-                    if (this.current_round +1 > this.rounds){
-                        this.finished = true;
-                    }
-                    else{
-                        this.current_round ++;
-                        this.current_player = 0;
-                    }
-                }
-
-                else{
-                    this.current_player ++;
-                }
-
-
-
-                this.timeLeft = this.maxTime;
-
-                this.sockets.emit("loadedDrawer", null);
-                this.sockets.emit("timeout");
-                this.sockets.emit("deleted");
-
-                let words = this.wordPicker();
-                this.players[this.current_player].emit('pickWords', words);
-
-                this.time_out = true;
-                setTimeout(()=>{
-                    this.time_out = false;
-                }, 2000)
-
+                this.round_over(this.countdown);
             }
         }, 1000);
+    }
+
+    round_over(){
+        clearInterval(this.countdown);
+        this.sockets.emit("nextRound");
+
+        let total_players = this.players.length;
+
+        if (this.current_player +1 >= total_players){
+            if (this.current_round +1 > this.rounds){
+                this.finished = true;
+            }
+            else{
+                this.current_round ++;
+                this.current_player = 0;
+            }
+        }
+
+        else{
+            this.current_player ++;
+        }
+
+        this.timeLeft = this.maxTime;
+        this.correct_guesses = 0;
+
+        this.sockets.emit("loadedDrawer", null);
+        this.sockets.emit("timeout");
+        this.sockets.emit("deleted");
+
+        let words = this.wordPicker();
+        this.players[this.current_player].emit('pickWords', words);
+
     }
 
     wordPicked(word){
@@ -73,13 +114,13 @@ class drawGame {
             word_length: word.word.length,
             time: this.maxTime
         };
-        console.log("emitting ", data);
         this.sockets.emit('setWord', data);
 
         this.time_clock();
     }
 
     wordPicker(){
+        console.log("picking from ", this.wordlist);
         let max = this.wordlist.length;
         let random = this.getRandomInt(max);
 
@@ -101,6 +142,90 @@ class drawGame {
 
     getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    guess(player, text) {
+        const name = player.username;
+        if (this.current_word !== null) {
+
+            if (player === this.players[this.current_player]) {
+                let bad_text = "I hob a kloans nidei weili probiert hob mei eigenes wort zu erroten haha!";
+                let message = {
+                    text: bad_text,
+                    author: name
+                };
+                this.sockets.emit("getMsg", message);
+                return;
+            }
+
+            if (text === this.current_word.word) {
+                this.correct_guess(player);
+            } else {
+                let message = {
+                    text: text,
+                    author: name
+                };
+                player.broadcast.emit("getMsg", message);
+            }
+
+        } else {
+            let message = {
+                text: text,
+                author: name
+            };
+            player.broadcast.emit("getMsg", message);
+        }
+    }
+
+    correct_guess(player){
+        let text = player.username + ' hat das Wort erraten!';
+        let message = {
+            text: text,
+            author: "Server"
+        };
+        this.sockets.emit("getMsg", message);
+
+        let points = this.calcPoints();
+        this.scores[player.username] += points;
+
+        this.correct_guesses ++;
+        if(this.correct_guesses === this.total_players -1)
+            this.wordGuessed();
+
+        let data={
+            player: player.username,
+            points: points
+        };
+
+        this.sockets.emit("addPoints", data);
+    }
+
+    calcPoints(){
+        let max_points = 500;
+        let step = 50;
+
+        let reduce = max_points / this.total_players;
+        let time_factor = this.timeLeft / this.maxTime;
+        let points = max_points - (this.correct_guesses * reduce);
+        points *= time_factor;
+
+        let foo = Math.floor(points / step);
+        return foo * step ;
+    }
+
+    wordGuessed(){
+        //todo: emit word solved event;
+        console.log("jeder hats erraten!");
+        console.log(this.scores);
+
+        let text = 'Jeder hat das Wort erraten!';
+        let message = {
+            text: text,
+            author: "Server"
+        };
+        this.sockets.emit("getMsg", message);
+
+        this.round_over();
     }
 
 }
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const socket = require('socket.io');
 
 const {drawGame} = require('./drawGame');
 
+require('dotenv').config(); // todo: remove in production
+
 const app = express();  // init server
 const server = app.listen(process.env.PORT || 3000 );  // run server
 
@@ -12,6 +14,7 @@ let users = []; // connected users
 let lines = []; // store the current painting
 
 let Chat = [];
+
 
 let Game = {
     started: false,
@@ -23,14 +26,14 @@ let Game = {
     maxTime: 30,
     wordlist: [
         {
-            word: 'amgelo',
+            word: 'hippogreif',
             category: 'funny',
-            author: 'Markus'
+            author: 'Julia'
         },
         {
             word: 'himbeere',
             category: 'normal',
-            author: 'Herbert'
+            author: 'Markus'
         },
         {
             word: 'bärtierchen',
@@ -47,15 +50,29 @@ io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
 
-    users.push(socket);
+    // users.push(socket);
 
     socket.on('createGame', function() {
         Game.started = true;
-        Game.host = users[0]; //todo: get host from emit data
+        // put player who started on pos 1 (host);
+        let i = users.indexOf(socket);
+        let temp = users[i];
+        users[i] = users[0];
+        users[0] = temp;
+
+        console.log(users[i].username, "Starting new Game with host: ", users[0].username, " \n Total players: ", users.length);
+
+        Game.host = users[0];
         Game.socket = socket;
         Game.sockets = io.sockets;
         Game.players = users;
-        socket.broadcast.emit('gameCreated');
+
+        // set first drawer to the person who started the game (todo: order array new)
+        Game.drawer = users.indexOf(socket);
+
+        // notify everbody the game has started
+        io.sockets.emit('lobby', false);
+
         //create new game instance
         current_game = new drawGame(Game);
         let words = current_game.wordPicker();
@@ -75,51 +92,52 @@ function newConnection(socket) {
             length: word.word.length,
             time: current_game.maxTime
         };
-        console.log("emitting setWord", data);;
         socket.emit('drawTimer', current_game.maxTime);
     });
 
 
     socket.on('sendMsg', function(message) {
        current_game.chat.push(message);
-
-        socket.broadcast.emit('getMsg', message);
+       current_game.guess(users[users.indexOf(socket)], message.text);
     });
 
     socket.on('disconnect', function() {
         let i = users.indexOf(socket);
-        const username = users[i].username;
-        users.splice(i, 1);
-
-        console.log('Got disconnect! ', username);
-
-        socket.broadcast.emit('removeUser', username);
+        if (i !== -1) {
+            const username = users[i].username;
+            users.splice(i, 1);
+            console.log('Got disconnect! ', username);
+            socket.broadcast.emit('removeUser', username);
+        }
     });
 
     socket.on('join', (data)=>{
 
+        users.push(socket);
         console.log('got connect! ', data.username);
-
         let i = users.indexOf(socket);
         users[i].username = data.username;
         socket.broadcast.emit('addUser', data.username);
     });
 
     socket.on('load', ()=>{
-        if(current_game === null)
-            return;
+        if(current_game !== null) {
+            socket.emit('loaded', current_game.lines);
+            socket.emit('loadedChat', current_game.chat);
+            socket.emit('loadedDrawer', users[current_game.current_player].username);
+            socket.emit('loadedScores', current_game.scores);
+            socket.emit('lobby', false);
+        }
 
-        socket.emit('loaded', current_game.lines);
+        else{
+            socket.emit('lobby', true);
+        }
 
         let user_names = [];
         for (let u in users){
             user_names.push(users[u].username);
-            console.log(users[u].username);
         }
         socket.emit('loadedUsers', user_names);
-        socket.emit('loadedChat', current_game.chat);
-
-        socket.emit('loadedDrawer', users[current_game.current_player].username);
 
     });
 
